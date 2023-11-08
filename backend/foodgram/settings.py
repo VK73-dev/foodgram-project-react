516f27eb724a2292e53fac4716ddda9c3190b60f
Кораблев Вячеслав <ua-wef@yandex.ru>
2023-11-08 17:55:49 +0300

    Add workflows and hosts

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', ' ')
 
 DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 
 
 INSTALLED_APPS = [
